7bde9d8cce9ad95b6253cd122447c03089146f36
Ife Runsewe <ife.runsewe@simplybusiness.co.uk>
2017-12-05 00:02:22 +0000

    Revised decipher

--- lib/decipher.rb
+++ lib/decipher.rb
@@ -1,71 +1,20 @@
-require_relative 'transformer'
-
-class Decipher
-  def initialize
-    @transformer = Transformer.new
-  end
-
-  def choose_task(line)
+module Decipher
+  def self.choose_task(line)
     case
-    when choose_create_image?(line)
-      number_of_columns = line.split(' ')[1]
-      number_of_rows = line.split(' ')[2]
-      @transformer.create_image(number_of_columns, number_of_rows)
-    when choose_colour_pixel?(line)
-      x = line.split(' ')[1]
-      y = line.split(' ')[2]
-      colour = line.split(' ')[3]
-      @transformer.colour_pixel(colour, x, y)
-    when choose_colour_vertical?(line)
-      x = line.split(' ')[1]
-      y1 = line.split(' ')[2]
-      y2 = line.split(' ')[3]
-      colour = line.split(' ').last
-      @transformer.colour_vertical(colour, x, y1, y2)
-    when choose_colour_horizontal?(line)
-      x1 = line.split(' ')[1]
-      x2 = line.split(' ')[2]
-      y = line.split(' ')[3]
-      colour = line.split(' ').last
-      @transformer.colour_horizontal(colour, x1, x2, y)
-    when choose_display_image?(line)
-      puts @transformer.image
-    when choose_clear_image?(line)
-      @transformer.clear_image
+    when line.start_with?('I')
+      'create_image'
+    when line.start_with?('L')
+      'colour_pixel'
+    when line.start_with?('V')
+      'colour_vertical'
+    when line.start_with?('H')
+      'colour_horizontal'
+    when line == 'S'
+      'display_image'
+    when line == 'C'
+      'clear_image'
     else
       puts 'unrecognised command :('
     end
-  end
-
-
-  private
-
-  def choose_create_image?(line)
-    display_if_condition_met(line.start_with?('I'), 'Creating image')
-  end
-
-  def choose_colour_pixel?(line)
-    display_if_condition_met(line.start_with?('L'), 'Colouring pixel')
-  end
-
-  def choose_colour_vertical?(line)
-    display_if_condition_met(line.start_with?('V'), 'Colouring vertical')
-  end
-
-  def choose_colour_horizontal?(line)
-    display_if_condition_met(line.start_with?('H'), 'Colouring horizontal')
-  end
-
-  def choose_display_image?(line)
-    display_if_condition_met(line == 'S', 'Displaying image')
-  end
-
-  def choose_clear_image?(line)
-    display_if_condition_met(line == 'C', 'Clearing image')
-  end
-
-  def display_if_condition_met(condition, message)
-    puts message if condition
-    condition
   end
 end
--- spec/decipher_spec.rb
+++ spec/decipher_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Decipher do
-  subject(:decipher) { described_class.new }
+  subject(:decipher) { described_class }
 
   context 'choosing a task' do
     subject(:choose_task) { decipher.choose_task(line) }
@@ -19,61 +19,42 @@ describe Decipher do
     context 'when it is create image' do
       let(:line){ command_to_create_image }
       it 'chooses the correct task' do
-        expect(STDOUT).to receive(:puts).with('Creating image')
-        choose_task
-      end
-    end
-
-    context 'when it is create image' do
-      let(:line) { 'I 5 6' }
-      it 'chooses the correct task' do
-        expect(STDOUT).to receive(:puts).with('Creating image')
-        choose_task
+        expect(choose_task).to eq('create_image')
       end
     end
 
     context 'when it is colour a pixel' do
       let(:line) { 'L 1 3 A' }
       it 'chooses the correct task' do
-        decipher.choose_task(command_to_create_image)
-        expect(STDOUT).to receive(:puts).with('Colouring pixel')
-        choose_task
+        expect(choose_task).to eq('colour_pixel')
       end
     end
 
     context 'when it is colour a vertical' do
       let(:line) { 'V 2 3 6 W' }
       it 'chooses the correct task' do
-        decipher.choose_task(command_to_create_image)
-        expect(STDOUT).to receive(:puts).with('Colouring vertical')
-        choose_task
+        expect(choose_task).to eq('colour_vertical')
       end
     end
 
     context 'when it is colour a horizontal' do
       let(:line) { 'H 3 5 2 Z' }
       it 'chooses the correct task' do
-        decipher.choose_task(command_to_create_image)
-        expect(STDOUT).to receive(:puts).with('Colouring horizontal')
-        choose_task
+        expect(choose_task).to eq('colour_horizontal')
       end
     end
 
     context 'when it is display image' do
       let(:line) { 'S' }
       it 'chooses the correct task' do
-        decipher.choose_task(command_to_create_image)
-        expect(STDOUT).to receive(:puts).with('Displaying image')
-        choose_task
+        expect(choose_task).to eq('display_image')
       end
     end
 
     context 'when it is clear image' do
       let(:line) { 'C' }
       it 'chooses the correct task' do
-        decipher.choose_task(command_to_create_image)
-        expect(STDOUT).to receive(:puts).with('Clearing image')
-        choose_task
+        expect(choose_task).to eq('clear_image')
       end
     end
   end
