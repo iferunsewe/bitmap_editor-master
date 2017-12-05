eeeb190426177811422bde3d8ba33bfc239318c9
Ife Runsewe <ife.runsewe@simplybusiness.co.uk>
2017-12-05 00:11:27 +0000

    Added transformer

--- lib/transformer.rb
+++ lib/transformer.rb
@@ -1,21 +1,66 @@
 class Transformer
-  def initialize(task, arguments={})
+  attr_reader :image
 
+  def initialize
+    @image = []
+    @matrix = []
   end
 
-  def create_image(number_of_columns, number_of_rows)
-
+  def create_image(line)
+    number_of_columns = line.split(' ')[1]
+    number_of_rows = line.split(' ')[2]
+    @matrix = Array.new(number_of_rows.to_i) { Array.new(number_of_columns.to_i, 'O') }
+    join_image
   end
 
-  def colour_pixel(colour, x, y)
-
+  def colour_pixel(line)
+    raise_error_if_image_empty?
+    x = line.split(' ')[1]
+    y = line.split(' ')[2]
+    colour = line.split(' ')[3]
+    @matrix[y.to_i - 1][x.to_i - 1] = colour
+    join_image
   end
 
-  def colour_vertical(colour, x1, x2)
-
+  def colour_vertical(line)
+    raise_error_if_image_empty?
+    x = line.split(' ')[1]
+    y1 = line.split(' ')[2]
+    y2 = line.split(' ')[3]
+    colour = line.split(' ').last
+    y1.to_i.upto(y2.to_i) do |y|
+      @matrix[y.to_i - 1][x.to_i - 1] = colour
+    end
+    join_image
   end
 
-  def colour_horizontal(colour, x1, x2)
+  def colour_horizontal(line)
+    raise_error_if_image_empty?
+    x1 = line.split(' ')[1]
+    x2 = line.split(' ')[2]
+    y = line.split(' ')[3]
+    colour = line.split(' ').last
+    x1.to_i.upto(x2.to_i) do |x|
+      @matrix[y.to_i - 1][x.to_i - 1] = colour
+    end
+    join_image
+  end
 
+  def display_image
+    puts @image
+  end
+
+  def clear_image
+    @image.clear
+  end
+
+  private
+
+  def join_image
+    @image = @matrix.map { |row| row.join }
+  end
+
+  def raise_error_if_image_empty?
+    raise "You must create image first. Try adding the command 'I <columns> <rows>'" if @image.empty?
   end
 end
--- spec/transformer_spec.rb
+++ spec/transformer_spec.rb
@@ -1,5 +1,71 @@
 require 'spec_helper'
 
 describe Transformer do
+  subject(:transformer) { described_class.new }
 
+  context 'creating an image' do
+    it 'returns an image' do
+      transformer.create_image('I 5 6')
+      expect(transformer.image).not_to be_empty
+    end
+  end
+
+  context 'when an image has already been created' do
+    before do
+      transformer.create_image('I 5 6')
+    end
+
+    context 'colouring a pixel' do
+      let(:x) { 2 }
+      let(:y) { 5 }
+      let(:colour) { 'R' }
+      let(:line) {"L #{x} #{y} #{colour}"}
+      it 'colours the correct pixel' do
+        transformer.colour_pixel(line)
+        expect(transformer.image[y - 1][x - 1]).to eq(colour)
+      end
+    end
+
+    context 'colour a vertical' do
+      let(:x) { 2 }
+      let(:y1) { 1 }
+      let(:y2) { 5 }
+      let(:colour) { 'T' }
+      let(:line) {"V #{x} #{y1} #{y2} #{colour}"}
+      it 'colours a vertical' do
+        transformer.colour_vertical(line)
+        y1.upto(y2) do |y|
+          expect(transformer.image[y - 1][x - 1]).to eq(colour)
+        end
+      end
+    end
+
+    context 'colour a horizontal' do
+      let(:x1) { 2 }
+      let(:x2) { 6 }
+      let(:y) { 5 }
+      let(:colour) { 'T' }
+      let(:line) {"V #{x1} #{x2} #{y} #{colour}"}
+      it 'colours a horizontal' do
+        transformer.colour_horizontal(line)
+        x1.upto(x2) do |x|
+          expect(transformer.image[y - 1][x - 1]).to eq(colour)
+        end
+      end
+    end
+
+    context 'displaying an image' do
+      it 'puts an image' do
+        expect(STDOUT).to receive(:puts).with(transformer.image)
+        transformer.display_image
+      end
+    end
+
+    context 'clearing an image' do
+      it 'clears an imaage' do
+        transformer.clear_image
+        expect(transformer.image).to be_empty
+      end
+    end
+  end
 end
